7125c4db0a2e288ea87c7c2f41824f1fb2f38b52
Admin <jorge.lopezbauer@ing.austral.edu.ar>
2020-05-03 19:57:56 -0300

    egdxg

--- src/SubwaySimulation.java
+++ src/SubwaySimulation.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SubwaySimulation {
 
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         LocalTime startingTime = LocalTime.now();
         System.out.print("Price: ");
         float price = scanner.nextFloat();
